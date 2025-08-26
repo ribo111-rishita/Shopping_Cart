422f9716dffa5dc1e4f5ebe919666ddf794a1c68
rishita.b006@gmail.com <rishitaboisnobi@Rishitas-MacBook-Pro.local>
2025-08-26 11:21:52 +0530

    holllaaa

--- dk lab/dklab/Sum-of-the-Numbers---fs-module---3---Node.js-fs-Module-Node.js-Synchronous-File---4gajiwchevyn/src/index.js
+++ dk lab/dklab/Sum-of-the-Numbers---fs-module---3---Node.js-fs-Module-Node.js-Synchronous-File---4gajiwchevyn/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const fs = require("fs")
+const data = fs.readFileSync("./input.txt","utf-8")
+const lines = data.split("\n")
+let total = 0;
+for(let index of lines){
+    const number = Number(index.split("")[1])
+    total+=number
+}
+console.log(total)
