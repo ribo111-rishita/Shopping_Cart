e2bb1b62b9c447cb73ab10e629d8e5f2c1dc76d7
rishita.b006@gmail.com <rishitaboisnobi@Rishitas-MacBook-Pro.local>
2025-09-02 11:35:31 +0530

    hii

--- dk lab/dklab/Sum-of-the-Numbers---fs-module---3---Node.js-fs-Module-Node.js-Synchronous-File---4gajiwchevyn/src/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-const data = fs.readFileSync("./input.txt","utf-8")
-const lines = data.split("\n")
-let total = 0;
-for(let index of lines){
-    const number = Number(index.split("")[1])
-    total+=number
-}
-console.log(total)
